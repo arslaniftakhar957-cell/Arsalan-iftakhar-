2215a5282c43596764ae6da8598161a203f2edba
arslaniftakhar957-cell <arslaniftakhar957@gmail.com>
2026-09-10 18:39:16 +0500

    Create posts.js

---
+++ blog/posts.js
@@ -0,0 +1,5 @@
+let blogPosts = [
+  { title: "1. How I Got My First 1000 Subscribers in 2026", link: "blog/post-1.html" },
+  { title: "2. 10 Viral TikTok Hashtag Strategies", link: "blog/post-2.html" },
+  { title: "3. Why Your YouTube Titles Are Killing Your Views", link: "blog/post-3.html" }
+];
