e3f76471310f752badb3860bfdea94bfa722e7b0
arslaniftakhar957-cell <arslaniftakhar957@gmail.com>
2026-09-15 11:17:04 +0500

    Update posts.js

--- blog/posts.js
+++ blog/posts.js
@@ -1,5 +1,15 @@
 let blogPosts = [
-  { title: "1. How I Got My First 1000 Subscribers in 2026", link: "blog/post-1.html" },
-  { title: "2. 10 Viral TikTok Hashtag Strategies", link: "blog/post-2.html" },
-  { title: "3. Why Your YouTube Titles Are Killing Your Views", link: "blog/post-3.html" }
+  { title: "How I Got My First 1000 Subscribers in 2026", link: "blog/post-1.html", category: "YouTube & Social Media Growth" },
+  { title: "10 Viral TikTok Hashtag Strategies", link: "blog/post-2.html", category: "YouTube & Social Media Growth" },
+  { title: "Why Your YouTube Titles Are Killing Your Views", link: "blog/post-3.html", category: "YouTube & Social Media Growth" },
+];
+
+let allCategories = [
+  "AI Tools & Productivity",
+  "Make Money Online & Finance",
+  "YouTube & Social Media Growth",
+  "Education & Career",
+  "Tech & Software Reviews",
+  "Health & Wellness",
+  "Lifestyle & Travel"
 ];
